39064d4fc3812423041fd7b60687a015395be37a
Shishir <theshishir01@gmail.com>
2024-10-31 11:58:31 -0500

    fixes

--- disposal-guide.user.js
+++ disposal-guide.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Disposal J.A.R.V.I.S.
 // @namespace   disposal-guide.nao.zero
-// @version     1.04
+// @version     1.05
 // @description color disposal options based on safety
 // @author      Terekhov, nao
 // @match       https://www.torn.com/loader.php?sid=crimes*
@@ -10,129 +10,125 @@
 // ==/UserScript==
 let url = window.location.href;
 const colors = {
-  safe: "#40Ab24",
-  moderatelySafe: "#A4D497",
-  caution: "#D6BBA2",
-  unsafe: "#B51B1B",
+    safe: "#40Ab24",
+    moderatelySafe: "#A4D497",
+    caution: "#D6BBA2",
+    unsafe: "#B51B1B",
 };
 const JOB_METHOD_DIFFICULTIES_MAP = {
-  "Biological Waste": {
-    safe: ["Sink"],
-    moderatelySafe: [],
-    caution: ["Burn"],
-    unsafe: ["Bury"],
-  },
-  "Body Part": {
-    safe: ["Dissolve"],
-    moderatelySafe: [],
-    caution: [],
-    unsafe: [],
-  },
-  "Broken Appliance": {
-    safe: ["Sink"],
-    moderatelySafe: [],
-    caution: ["Abandon", "Bury"],
-    unsafe: ["Dissolve"],
-  },
-  "Building Debris": {
-    safe: ["Sink"],
-    moderatelySafe: [],
-    caution: ["Abandon", "Bury"],
-    unsafe: [],
-  },
-  "Dead Body": {
-    safe: ["Dissolve"],
-    moderatelySafe: [],
-    caution: [],
-    unsafe: [],
-  },
-  Documents: {
-    safe: ["Burn"],
-    moderatelySafe: [],
-    caution: ["Abandon", "Bury"],
-    unsafe: ["Dissolve", "Sink"],
-  },
-  Firearm: {
-    safe: ["Sink"],
-    moderatelySafe: ["Bury"],
-    caution: [],
-    unsafe: ["Dissolve"],
-  },
-  "General Waste": {
-    safe: ["Burn"],
-    moderatelySafe: ["Bury"],
-    caution: ["Abandon", "Sink"],
-    unsafe: ["Dissolve"],
-  },
-  "Industrial Waste": {
-    safe: ["Sink"],
-    moderatelySafe: [],
-    caution: ["Abandon", "Bury"],
-    unsafe: [],
-  },
-  "Murder Weapon": {
-    safe: ["Sink"],
-    moderatelySafe: [],
-    caution: [],
-    unsafe: ["Dissolve"],
-  },
-  "Old Furniture": {
-    safe: ["Burn"],
-    moderatelySafe: [],
-    caution: ["Abandon", "Bury", "Sink"],
-    unsafe: ["Dissolve"],
-  },
-  Vehicle: {
-    safe: ["Sink"],
-    moderatelySafe: ["Burn"],
-    caution: ["Abandon"],
-    unsafe: [],
-  },
+    "Biological Waste": {
+        safe: ["Sink"],
+        moderatelySafe: [],
+        caution: ["Burn"],
+        unsafe: ["Bury"],
+    },
+    "Body Part": {
+        safe: ["Dissolve"],
+        moderatelySafe: [],
+        caution: [],
+        unsafe: [],
+    },
+    "Broken Appliance": {
+        safe: ["Sink"],
+        moderatelySafe: [],
+        caution: ["Abandon", "Bury"],
+        unsafe: ["Dissolve"],
+    },
+    "Building Debris": {
+        safe: ["Sink"],
+        moderatelySafe: [],
+        caution: ["Abandon", "Bury"],
+        unsafe: [],
+    },
+    "Dead Body": {
+        safe: ["Dissolve"],
+        moderatelySafe: [],
+        caution: [],
+        unsafe: [],
+    },
+    Documents: {
+        safe: ["Burn"],
+        moderatelySafe: [],
+        caution: ["Abandon", "Bury"],
+        unsafe: ["Dissolve", "Sink"],
+    },
+    Firearm: {
+        safe: ["Sink"],
+        moderatelySafe: ["Bury"],
+        caution: [],
+        unsafe: ["Dissolve"],
+    },
+    "General Waste": {
+        safe: ["Burn"],
+        moderatelySafe: ["Bury"],
+        caution: ["Abandon", "Sink"],
+        unsafe: ["Dissolve"],
+    },
+    "Industrial Waste": {
+        safe: ["Sink"],
+        moderatelySafe: [],
+        caution: ["Abandon", "Bury"],
+        unsafe: [],
+    },
+    "Murder Weapon": {
+        safe: ["Sink"],
+        moderatelySafe: [],
+        caution: [],
+        unsafe: ["Dissolve"],
+    },
+    "Old Furniture": {
+        safe: ["Burn"],
+        moderatelySafe: [],
+        caution: ["Abandon", "Bury", "Sink"],
+        unsafe: ["Dissolve"],
+    },
+    Vehicle: {
+        safe: ["Sink"],
+        moderatelySafe: ["Burn"],
+        caution: ["Abandon"],
+        unsafe: [],
+    },
 };
 const NERVE_COST_BY_METHOD = {
-  Abandon: 6,
-  Bury: 8,
-  Burn: 10,
-  Sink: 12,
-  Dissolve: 14,
+    Abandon: 6,
+    Bury: 8,
+    Burn: 10,
+    Sink: 12,
+    Dissolve: 14,
 };
 
 function insert() {
-  $(".crime-option:not([processed])").each(function () {
-    const crimeName = $(
-      $("div[class^='crimeOptionSection']", $(this))[0],
-    ).text();
-    if (JOB_METHOD_DIFFICULTIES_MAP[crimeName]) {
-      for (let crimeDifficulty in JOB_METHOD_DIFFICULTIES_MAP[crimeName]) {
-        let crimeColor = colors[crimeDifficulty];
-        for (let method of JOB_METHOD_DIFFICULTIES_MAP[crimeName][
-          crimeDifficulty
-        ]) {
-          $(`button[aria-label='${method}']`, $(this)).css(
-            "border",
-            `3px solid ${crimeColor}`,
-          );
-            if (crimeDifficulty == "safe"){
-                $(`button[aria-label='${method}']`, $(this)).trigger('click');
+    url = window.location.href;
 
+    if (!url.includes("disposal")){
+        return;
+    }
+
+    $(".crime-option:not([processed])").each(function () {
+        const crimeName = $(
+            $("div[class^='crimeOptionSection']", $(this))[0],
+        ).text();
+        if (JOB_METHOD_DIFFICULTIES_MAP[crimeName]) {
+            for (let crimeDifficulty in JOB_METHOD_DIFFICULTIES_MAP[crimeName]) {
+                let crimeColor = colors[crimeDifficulty];
+                for (let method of JOB_METHOD_DIFFICULTIES_MAP[crimeName][
+                    crimeDifficulty
+                ]) {
+                    $(`button[aria-label='${method}']`, $(this)).css(
+                        "border",
+                        `3px solid ${crimeColor}`,
+                    );
+                    if (crimeDifficulty == "safe"){
+                        $(`button[aria-label='${method}']`, $(this)).trigger('click');
+
+                    }
+                }
             }
         }
-      }
-    }
-    $(this).attr("processed", "true");
-  });
-
-    setTimeout(insert, 1000);
+        $(this).attr("processed", "true");
+    });
 }
 
-if (url.includes("disposal")){
-    insert();
-}
 
-$(window).on("hashchange", function (e) {
-    url = window.location.href;
-    if (url.includes("disposal")) {
-        insert();
-    }
-});
+setInterval(insert, 1000);
 
