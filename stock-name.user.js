0c3508ea03e2dc9a9c9a83a8d88160d10e6968e5
Shishir <theshishir01@gmail.com>
2024-11-18 13:59:21 -0600

    stock update

--- stock-name.user.js
+++ stock-name.user.js
@@ -7,7 +7,46 @@
 // @match        https://www.torn.com/page.php?sid=stocks*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
 // @grant        none
+// @run-at        document-start
 // ==/UserScript==
+
+let stocksData = {};
+const { fetch: origFetch } = window;
+window.fetch = async (...args) => {
+  //  console.log("fetch called with args:", args);
+
+  const response = await origFetch(...args);
+  console.log("STOCKNAME", response);
+
+  /* work with the cloned response in a separate promise
+     chain -- could use the same chain with `await`. */
+
+  if (response.url.includes("StockMarket")) {
+    //  console.log("REsponseL : "+response);
+
+    response
+      .clone()
+      .json()
+      .then(function (body) {
+        if (body.success) {
+          for (const stockData of body.stocks) {
+            const symbol = stocksData.profile.acronym;
+            const currentPrice = stockData.sharesPrice.chartData[1].value;
+            const transactions = stockData.userOwned.transactions;
+
+            stocksData[stocksData.id] = {
+              symbol: symbol,
+              current_price: currentPrice,
+              transactions: transactions,
+            };
+          }
+        }
+      })
+      .catch((err) => console.error(err));
+  }
+  change();
+  return response;
+};
 
 function change() {
   if ($("ul[class^='stock_']").length == 0) {
@@ -26,5 +65,3 @@ function change() {
       .append(` <div style="color:yellow;"> [${sym}]</div>`);
   });
 }
-
-change();
