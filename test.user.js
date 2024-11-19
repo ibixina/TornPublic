d3f12e70f84075150135860bf56ba349a4ea76a3
Shishir <theshishir01@gmail.com>
2024-11-19 01:02:14 -0600

    testing

---
+++ test.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         testing
+// @namespace    testing.zero.nao
+// @version      0.1
+// @description  try to tke over the world!
+// @author       nao [2669774]
+// @match        https://www.torn.com*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
+// @grant        none
+// @updateURL    <UPDATE_URL>
+// @downloadURL  <DOWNLOAD_URL>
+
+// ==/UserScript==
+
+let api = "";
+let url = window.location.href;
+let rfc = getRFC();
+
+function getRFC() {
+  var rfc = $.cookie("rfc_v");
+  if (!rfc) {
+    var cookies = document.cookie.split("; ");
+    for (var i in cookies) {
+      var cookie = cookies[i].split("=");
+      if (cookie[0] == "rfc_v") {
+        return cookie[1];
+      }
+    }
+  }
+  return rfc;
+}
+
+console.log("test");
