3dd8234cdcb07adb9d48491e2e47d84ee0e4b19f
Shishir <theshishir01@gmail.com>
2024-11-23 21:03:37 -0600

    added feature to hide fire button

--- company-trainer.user.js
+++ company-trainer.user.js
@@ -9,7 +9,6 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-
 GM_addStyle(`
 .trainOrder {
     width: 100%;
@@ -19,121 +18,124 @@ GM_addStyle(`
 
 let trains = getData();
 
-
-function getData(){
-    let tempValue = localStorage.trainsOrdered || "{}";
-    tempValue = JSON.parse(tempValue);
-    return tempValue;
+function getData() {
+  let tempValue = localStorage.trainsOrdered || "{}";
+  tempValue = JSON.parse(tempValue);
+  return tempValue;
 }
 
-function updateData(){
-    console.log(trains);
-    localStorage.trainsOrdered = JSON.stringify(trains);
+function hideButton() {
+  const currentStatus = $(".fire").css("visibility");
+  let displayStatus = currentStatus === "hidden" ? "visible" : "hidden";
+
+  GM_addStyle(`
+.fire {
+    visibility: ${displayStatus};
+}`);
+}
+
+function updateData() {
+  console.log(trains);
+  localStorage.trainsOrdered = JSON.stringify(trains);
 }
 
 function update() {
+  trains = getData();
+  $(".trainOrder").each(function () {
+    let val = $(this).attr("value") || 0;
+    console.log(`Value ${val}`);
+    let userId = $(this).attr("userid");
 
-    trains = getData();
-    $(".trainOrder").each(function () {
-        let val = $(this).attr("value") || 0;
-        console.log(`Value ${val}`);
-        let userId = $(this).attr("userid");
-
-        trains[userId] = val;
-    });
-    console.log(trains);
-    updateData();
-    updateDisplayValue();
-
+    trains[userId] = val;
+  });
+  console.log(trains);
+  updateData();
+  updateDisplayValue();
 }
 
 function updateDisplayValue() {
-    let currTrains = getData();
+  let currTrains = getData();
 
+  $(".trainOrder").each(function () {
+    let uid = $(this).attr("userid");
+    let nTrains = currTrains[uid] || 0;
 
-    $(".trainOrder").each(function () {
-        let uid = $(this).attr("userid");
-        let nTrains = currTrains[uid] || 0;
-
-        $(this).attr("value", parseInt(nTrains))
-
-
-    });
-
+    $(this).attr("value", parseInt(nTrains));
+  });
 }
 
 function train(id) {
-    console.log(`Training ${id}`);
-    $.post(`https://www.torn.com/companies.php?rfcv=${getRFC()}`, {
-        step: "trainemp2",
-        ID: id
-    }, function (response) {
-        response = JSON.parse(response);
-        if (response.success) {
-            trains[id]--;
-        }
-        if (trains[id] <= 0) {
-            $(".train", $(`li[data-user="${id}"]`)).html("N/A");
-
-        }
-        updateData();
-        updateDisplayValue();
-        console.log(response);
-    });
-
-
+  console.log(`Training ${id}`);
+  $.post(
+    `https://www.torn.com/companies.php?rfcv=${getRFC()}`,
+    {
+      step: "trainemp2",
+      ID: id,
+    },
+    function (response) {
+      response = JSON.parse(response);
+      if (response.success) {
+        trains[id]--;
+      }
+      if (trains[id] <= 0) {
+        $(".train", $(`li[data-user="${id}"]`)).html("N/A");
+      }
+      updateData();
+      updateDisplayValue();
+      console.log(response);
+    },
+  );
 }
 
 function insert() {
-    let url = window.location.href;
-    if (!url.includes("employees")) {
-        return;
+  let url = window.location.href;
+  if (!url.includes("employees")) {
+    return;
+  }
 
-    }
-
-    if ($(".employee-list > li").length > 0 && $('a[href^="?step=trainemp"]').length > 0) {
-        $(".employee-list > li").each(function () {
-            let userid = $(this).attr("data-user");
-            $(".days", $(this)).html(`<input type="text" userid="${userid}" class="trainOrder">`);
-            if (trains[userid] && trains[userid] > 0) {
-                $(".train", $(this)).html(`<button class="torn-btn" id="train${userid}">T</button>`);
-                $(`#train${userid}`).on("click", function () {
-                    train(userid);
-
-                });
-
-
-            }
-            else {
-                $(".train", $(this)).html("N/A");
-
-
-            }
+  if (
+    $(".employee-list > li").length > 0 &&
+    $('a[href^="?step=trainemp"]').length > 0
+  ) {
+    $(".employee-list > li").each(function () {
+      let userid = $(this).attr("data-user");
+      $(".days", $(this)).html(
+        `<input type="text" userid="${userid}" class="trainOrder">`,
+      );
+      if (trains[userid] && trains[userid] > 0) {
+        $(".train", $(this)).html(
+          `<button class="torn-btn" id="train${userid}">T</button>`,
+        );
+        $(`#train${userid}`).on("click", function () {
+          train(userid);
         });
-        updateDisplayValue();
+      } else {
+        $(".train", $(this)).html("N/A");
+      }
+    });
+    updateDisplayValue();
+    //
+    //    $(".submit-changes").append(
+    //      `<button id="updateTrains" class="torn-btn">Update</button>
+    //<button id="hideButton" class="torn-btn">Toggle Fire</button>`,
+    //    );
 
-        $(".submit-changes").append(`<button id="updateTrains" class="torn-btn">Update</button>`);
-        $("#updateTrains").on("click", function(){
-            update();
-
-        });
-
-    }
-    setTimeout(insert, 1000);
+    $("div.title:nth-child(1)").append(
+      `<button id="updateTrains" class="torn-btn">Update Trains</button>
+      <button id="hideButton" class="torn-btn">Toggle Fire</button>`,
+    );
+    $("#updateTrains").on("click", function () {
+      update();
+    });
+    $("#hideButton").on("click", function () {
+      hideButton();
+    });
+  }
+  setTimeout(insert, 1000);
 }
 
-
-
-$(window).on('hashchange', function (e) {
-    insert();
-
+$(window).on("hashchange", function (e) {
+  insert();
 });
 
 insert();
-
-
-
-
-
-
-
