41e502fbf9512fef454a623a5d18271b37c29e2a
Shishir <theshishir01@gmail.com>
2025-01-04 17:27:07 -0600

    execute starting

---
+++ execute.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         execute
+// @namespace    execute.zero.nao
+// @version      0.1
+// @description  execute highlight
+// @author       nao [2669774]
+// @match        https://www.torn.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
+// @grant        none
+// @updateURL    <UPDATE_URL>
+// @downloadURL  <DOWNLOAD_URL>
+
+// ==/UserScript==
+
+let api = localStorage.getItem("api_full");
+let url = window.location.href;
+let rfc = getRFC();
+
+function getRFC() {
+  var rfc = $.cookie("rfc_v");
+  if (!rfc) {
+    var cookies = document.cookie.split("; ");
+    for (var i in cookies) {
+      var cookie = cookies[i].split("=");
+      if (cookie[0] == "rfc_v") {
+        return cookie[1];
+      }
+    }
+  }
+  return rfc;
+}
